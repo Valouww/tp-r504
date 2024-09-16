dbc1a52d480e6f47dcc3d0b4278cf2c6a21e5a6c
Valouww <valentinhelan@gmail.com>
2024-09-16 15:54:50 +0200

    update tests

--- tp1/fonctions.py
+++ tp1/fonctions.py
@@ -1,6 +1,6 @@
 def puissance(a,b):
-	if not type(a) is int or not type(b) is int:
-	raise TypeError("Only integers are allowed")
-	elif b <= 0:
-		raise TypeError("La puissance doit être possitive")
-	return a**b
+   if not type(a) is int or not type(b) is int:
+      raise TypeError("Only integers are allowed")
+   elif b <= 0:
+       raise TypeError("La puissance doit être possitive")
+   return a**b
