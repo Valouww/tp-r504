4d5bebf8822b2fa88724600352a54a51d6bf9141
Valouww <valentinhelan@gmail.com>
2024-10-07 16:19:39 +0200

    Modif de start foncionnel

--- TP-LB/tp-A/start.sh
+++ TP-LB/tp-A/start.sh
@@ -1,8 +1,8 @@
-docker network create --internal tplb
+docker network create tplb
 docker build . -t im-nginx-lb 
 mkdir shared{1..2}
 echo "<h1>Hello 1</h1>" > shared1/index.html
 echo "<h1>Hello 2</h1>" > shared2/index.html
-docker run --rm --name nginx1 -d --network=tplb -p 127.0.0.1:80:81 --mount type=bind,source=shared1/,target=/usr/share/nginx/html nginx:latest
-docker run --rm --nale nginx2 -d --network=tplb -p 127.0.0.1:80:81 --mount type=bind,source=shared2/,target=/usr/share/nginx/html nginx:latest
-docker run --rm -d --network=tplb -p 127.0.0.1:80:83 im-nginx-lb
+docker run --rm --name nginx1 -d --network=tplb -p 81:80 --mount type=bind,source=/home/valdav/tp-r504/TP-LB/tp-A/shared1/,target=/usr/share/nginx/html nginx:latest
+docker run --rm --name nginx2 -d --network=tplb -p 82:80 --mount type=bind,source=/home/valdav/tp-r504/TP-LB/tp-A/shared2/,target=/usr/share/nginx/html nginx:latest
+docker run --rm -d --network=tplb -p 83:80 im-nginx-lb
