fe1cccacbb7d3542503a0ade985462ce051618a5
Valouww <valentinhelan@gmail.com>
2024-10-07 17:26:53 +0200

    MAj erreur quote

--- TP-LB/tp-A/start.sh
+++ TP-LB/tp-A/start.sh
@@ -1,6 +1,6 @@
 docker network create tplb
-docker build . -t im-nginx-lb 
-mkdir shared{1..2}
+docker build . -t im-nginx-lb
+mkdir -p shared{1..2}
 echo "<h1>Hello 1</h1>" > shared1/index.html
 echo "<h1>Hello 2</h1>" > shared2/index.html
 docker run --rm --name nginx1 -d --network=tplb -p 81:80 --mount type=bind,source=/home/valdav/tp-r504/TP-LB/tp-A/shared1/,target=/usr/share/nginx/html nginx:latest
