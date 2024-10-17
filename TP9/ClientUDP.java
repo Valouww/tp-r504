45911fcaa40f6903e9632d94abe22eb013852a02
Valouww <valentinhelan@gmail.com>
2024-10-17 11:14:30 +0200

    Ajout Serveur et client TCP

--- TP9/ClientUDP.java
+++ TP9/ClientUDP.java
@@ -1,17 +1,29 @@
 import java.io.*;
 import java.net.*;
 
-public class ClientUDP
-{
-	public static void main( String[] args ) throws IOException
-	{
-		String s="Hello World";
-		byte [ ] data = s . getBytes ();
-		InetAddress addr = InetAddress.getLocalHost();
-		System.out.println("adresse=" +addr.getHostName());
-		DatagramPacket packet = new DatagramPacket (data, data.length, addr, 1234);
-		DatagramSocket sock = new DatagramSocket();
-		sock.send(packet);
-		sock.close();
-	}
+public class ClientUDP {
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            String s = "Hello World";
+            byte[] data = s.getBytes();
+            InetAddress addr = InetAddress.getLocalHost();
+            System.out.println("adresse = " + addr.getHostName());
+
+            DatagramPacket packet = new DatagramPacket(data, data.length, addr, 1234);
+            DatagramSocket sock = new DatagramSocket();
+            sock.send(packet);
+            
+            sock.receive (packet);
+			      String strrecep = new String (packet.getData());
+			      System.out.println("str=" + strrecep);
+            
+            sock.close();
+        } catch (Exception ex) 
+        {
+            System.out.println("Erreur: ");
+        }
+    }
 }
+
--- TP9/ServeurUDP.java
+++ TP9/ServeurUDP.java
@@ -1,18 +1,26 @@
 import java.io.*;
 import java.net.*;
 
-public class ServeurUDP
-{
-	public static void main( String[] args ) throws IOException
-	{
-		DatagramSocket sock = new DatagramSocket (1234); 
-		while (true)
-		{
-			System.out.println("-Waiting data");
-			DatagramPacket packet =  new DatagramPacket (new byte [1024], 1024);
-			sock.receive (packet);
-			String str = new String (packet.getData());
-			System.out.println("str=" + str);
-		}
-	}
+public class ServeurUDP {
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            DatagramSocket sock = new DatagramSocket (1234); 
+		        while (true)
+		        {
+			        System.out.println("-Waiting data");
+			        DatagramPacket packet =  new DatagramPacket (new byte [1024], 1024);
+			        sock.receive (packet);
+			        String str = new String (packet.getData());
+			        System.out.println("str=" + str);
+			        
+              sock.send(packet);
+		        }
+        } catch (Exception ex) 
+        {
+            System.out.println("Erreur: ");
+        }
+    }
 }
+
