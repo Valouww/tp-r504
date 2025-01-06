ba609d6cc6b199feaf5eeee5e4801a5b9a22c775
Valouww <valentinhelan@gmail.com>
2025-01-06 16:55:56 +0100

    Fin TP regex

--- tp4/app_1.py
+++ tp4/app_1.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from re import *
 import mysql.connector
 
 app = Flask(__name__)
@@ -12,11 +13,31 @@ db_config = {
 	'port': '3306'
 }
 
+@app.route("/form")
+def monForm():
+	return render_template("form.html")
+
 @app.route('/newuser',methods = ['POST', 'GET'])
 def saisie():
+	patt1=".{6,}"
+	patt2=".*\d.*"
+	patt3=".*([a-z])*.*[A-Z].*([a-z])*.*"
+	patt4=".*(#|%|\{|\}|@).*" 	
 	if request.method == 'POST':
-		res = request.form.get( "lname" )
-		return res.text()
+		res = request.form.get( "entree" )
+		if fullmatch( patt1, res ) == None:
+			return "La chaîne de caractère ne contient pas 6 caractères"
+		else:
+			if fullmatch( patt2, res ) == None:
+				return "La chaîne de caractère doit contenir au moins 1 chiffre"
+			else:
+				if fullmatch( patt3, res ) == None:
+					return "La chaîne de caractère doit contenir au moins 1 majuscule et 1 minuscule" 
+				else:
+					if fullmatch( patt4, res ) == None:
+						return "La chaîne de caractère doit contenir au moins 1 caractère parmi les 5 suivants : #%{}@"
+					else:
+						return "Le mot de passe est valide"
 
 
 
