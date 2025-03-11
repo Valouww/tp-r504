8e78ccbe4d60fb30d5d7991edbfaac76c3fa5d25
user <user@debian>
2025-03-11 06:21:18 -0400

    TP fini

--- nmap/scan_A106_v2.sh
+++ nmap/scan_A106_v2.sh
@@ -4,7 +4,6 @@ awk '{print $NF}' temp1.txt > temp2.txt
 echo "IP;TCP_OPEN;UDP_OPEN" > scan-result_2.csv
 while read VAR
 do
-   echo $VAR
    a=$(nmap -F $VAR |grep "/tcp" | grep "open"| wc -l)
    b=$(nmap -F -sU $VAR |grep "/udp" | grep "open"| wc -l)
    echo "$VAR;$a;$b" >> scan-result_2.csv
