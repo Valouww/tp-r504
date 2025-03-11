7d271401695ee25041214b197758fa529fcc0548
user <user@debian>
2025-03-11 06:17:48 -0400

    TP fini

--- nmap/scan_A106_v2.sh
+++ nmap/scan_A106_v2.sh
@@ -1,11 +1,13 @@
 nmap -sn 172.16.0.0/24 | grep "scan report" | tr -d '()'>temp1.txt
 awk '{print $NF}' temp1.txt > temp2.txt
 
-echo "IP;TCP_OPEN" > scan-result_1.csv
+echo "IP;TCP_OPEN;UDP_OPEN" > scan-result_2.csv
 while read VAR
 do
+   echo $VAR
    a=$(nmap -F $VAR |grep "/tcp" | grep "open"| wc -l)
-   echo "$VAR;$a" >> scan-result_1.csv
+   b=$(nmap -F -sU $VAR |grep "/udp" | grep "open"| wc -l)
+   echo "$VAR;$a;$b" >> scan-result_2.csv
 done < temp2.txt
 
 rm temp1.txt temp2.txt
